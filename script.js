7fcc8584ca5285d919ba87953e7d884decc0a60a
Álex Alencar <alexalencarr17@outlook.com>
2023-09-25 15:35:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const pikachu = document.querySelector('.pikachu');
+
+const jump = () => {
+    pikachu.classList.add('jump');
+    
+    setTimeout(() => {
+        pikachu.classList.remove('jump');
+    
+    }, 500);
+}
+
+document.addEventListener('keydown', jump);
